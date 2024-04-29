560e4f901fcaf87b232661ddd60483157c737a56
Sam <sjwong723@gmail.com>
2024-04-29 10:09:42 -0700

    9/29/24

--- app/src/main/java/com/example/gamblr/MainActivity.kt
+++ app/src/main/java/com/example/gamblr/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.gamblr
 
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gamblr.databinding.ActivityMainBinding
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var wins = 0
+    var spins = 0
+    var bet = 0
+    var coins = 0
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var icons : List<Int>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // todo: implement a bunch of icons using photoIDs
+
+        binding.button_spin.setOnClickListener {
+            // todo: to be implemented
+            // todo: randomly choose which icons get chosen
+            // todo: keep track of spins so each fifth gets 10% chance of win
+        }
+
+        binding.
     }
 }
