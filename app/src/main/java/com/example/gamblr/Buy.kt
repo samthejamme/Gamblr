deb37a63e3e23e7605b83f71f6e2c7f1ba264b21
Sam <sjwong723@gmail.com>
2024-05-23 09:33:18 -0700

    5/23/24

--- app/src/main/java/com/example/gamblr/Buy.kt
+++ app/src/main/java/com/example/gamblr/Buy.kt
@@ -1,6 +1,7 @@
 package com.example.gamblr
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gamblr.databinding.AddMoreMoneyBinding
@@ -12,6 +13,7 @@ class Buy : AppCompatActivity() {
     }
     lateinit var binding: AddMoreMoneyBinding
     var balance = 0
+    var purcahses = 0
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,25 +22,47 @@ class Buy : AppCompatActivity() {
         setContentView(binding.root)
 
         balance = intent.getIntExtra(BUY, 0)
+        binding.textViewScam.text = "$${purcahses}/$100"
 
         // todo: implement payment system that uses money
         binding.buttonGoldCoins.setOnClickListener {
             balance += 5
+            purcahses += 9
         }
         binding.buttonGoldCoins.setOnClickListener {
             balance += 10
+            purcahses += 19
         }
         binding.buttonGoldCoins.setOnClickListener {
             balance += 50
+            purcahses += 29
         }
         binding.buttonGoldCoins.setOnClickListener {
             balance += 70
+            purcahses += 39
         }
         binding.buttonGoldCoins.setOnClickListener {
             balance += 100
+            purcahses += 49
         }
         binding.buttonGoldCoins.setOnClickListener {
             balance += 200
+            purcahses += 99
+        }
+        binding.buttonScamCollector.setOnClickListener {
+            if (purcahses % 100 == 0) {
+                MainActivity.coins += 10
+                val alertDialogBuilder = AlertDialog.Builder(this)
+                alertDialogBuilder.setTitle("Congratulations")
+                alertDialogBuilder.setMessage("You just collected your free bonus coins!")
+
+                alertDialogBuilder.setPositiveButton(android.R.string.yes) { dialog, which -> }
+                alertDialogBuilder.show()
+            }
+        }
+        binding.floatingActionButtonAddMoneyClose.setOnClickListener {
+            MainActivity.coins += balance
+            finish()
         }
     }
 }
